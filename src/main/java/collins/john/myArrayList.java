df69d8bcfd20fad6911e44b085bbeeb23ae4c62e
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-02-22 15:08:04 -0500

    add and get myArray

---
+++ src/main/java/collins/john/myArrayList.java
@@ -0,0 +1,38 @@
+package collins.john;
+
+import java.util.Arrays;
+
+/**
+ * Created by johncollins on 2/22/17.
+ */
+public class myArrayList<T>
+{
+
+    private T[] temp;
+    private T[] holder;
+    private int mySize = 0;
+
+    public <T> myArrayList()
+    {
+        //does it need a constructor?
+    }
+
+    public int getMySize()
+    {
+        return mySize;
+    }
+
+    public void add(T t)
+    {
+        //copy holder, increase size, add new T at end, copy back to holder. mySize++
+        temp = Arrays.copyOf(holder, holder.length / 2 + holder.length);
+        temp[mySize] = t;
+        holder = Arrays.copyOf(temp, temp.length);
+        mySize++;
+
+    }
+    public T myGetAt(int i){
+        return holder[i];
+    }
+
+}
