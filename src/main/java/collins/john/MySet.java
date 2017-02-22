7ab49cbf34d89222a6aa2c724c8086f1738c48bc
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-02-22 17:14:39 -0500

    MySet, no test

--- src/main/java/collins/john/MySet.java
+++ src/main/java/collins/john/MySet.java
@@ -3,7 +3,13 @@ package collins.john;
 /**
  * Created by johncollins on 2/22/17.
  */
-public class MySet
+public class MySet<T> extends MyArrayList
 {
 
+public void myAddtoSet(T t)
+{
+    if(!this.myContains(t)){
+        this.myAdd(t);
+    }
+}
 }
---
+++ src/test/java/collins/john/MySetTest.java
@@ -0,0 +1,16 @@
+package collins.john;
+
+import org.junit.Before;
+
+/**
+ * Created by johncollins on 2/22/17.
+ */
+public class MySetTest
+{
+    MySet<String> testSet;
+    @Before
+    public void setUp(){
+        MySet<String> testSet = new MySet<>();
+
+    }
+}
