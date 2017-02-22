4555fc2b0de6e6fd99b3232b924722ef65b5cf72
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-02-22 17:45:43 -0500

    MySet basic complete with tests

--- src/main/java/collins/john/MySet.java
+++ src/main/java/collins/john/MySet.java
@@ -1,17 +1,100 @@
 package collins.john;
 
+import java.util.Arrays;
+
 /**
  * Created by johncollins on 2/22/17.
  */
-public class MySet<T> extends MyArrayList
+public class MySet<T>
 {
 
-    public void myAddtoSet(T t)
+    private T t;
+    private Object[] empty;
+    private Object[] temp;
+    private Object[] holder;
+    private int mySize;
+    private boolean myEmpty;
+    boolean contains;
+
+    public MySet()
     {
-        if (!this.myContains(t))
-        {
-            this.myAdd(t);
-        }
+        holder = new Object[10];
+        myEmpty = true;
+        mySize = 0;
     }
+
+    public int getMySize()
+    {
+        return mySize;
+    }
+
+    public void myAdd(T t)
+    {
+        this.doesContain(t);
+        if(!contains)
+        {
+            //copy holder, increase size, add new T at end, copy back to holder. mySize++
+            temp = Arrays.copyOf(holder, holder.length + 10);
+            temp[mySize] = t;
+            holder = Arrays.copyOf(temp, temp.length);
+            temp = empty;
+            mySize++;
+        }
+
+    }
+
+    public Object myGetAt(int i)
+    {
+        return holder[i];
+    }
+
+
+    public void myClear()
+    {
+        holder = empty;
+        mySize = 0;
+    }
+
+    public boolean isMyEmpty()
+    {
+        myEmpty = mySize == 0 ? true : false;
+        return myEmpty;
+    }
+
+    private void doesContain(T t)
+    {
+        contains = false;
+        //if any T in holder .equals t, return true
+        for (Object i : holder
+                )
+        {
+            if (t.equals(i))
+            {
+                contains = true;
+            }
+        }
+
+    }
+
+    public boolean myContains(T t)
+    {
+        this.doesContain(t);
+        return contains;
+        /*
+        boolean contains = false;
+        //if any T in holder .equals t, return true
+        for (Object i : holder
+                )
+        {
+            if (t.equals(i))
+            {
+                contains = true;
+            }
+        }
+        return contains;
+        */
+    }
+
+
 
 }
--- src/test/java/collins/john/MySetTest.java
+++ src/test/java/collins/john/MySetTest.java
@@ -3,6 +3,8 @@ package collins.john;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by johncollins on 2/22/17.
  */
@@ -11,11 +13,59 @@ public class MySetTest
     MySet<String> testSet;
     @Before
     public void setUp(){
-        MySet<String> testSet = new MySet<>();
+        testSet = new MySet<>();
 
     }
     @Test
     public void myAddToSetTest(){
-        testSet.add
+        testSet.myAdd("one");
+        testSet.myAdd("two");
+        testSet.myAdd("one");
+        int actual = testSet.getMySize();
+        //int actual = -1;//fail test
+        int expected = 2;
+        assertEquals("should be 2, no dup's allowed", expected, actual);
     }
+    @Test
+    public void isMyEmptyTest(){
+        boolean actual = testSet.isMyEmpty();
+        boolean expected = true;
+        //boolean expected = false;//fail test
+        assertEquals("should be true, new list is empty", expected, actual);
+    }
+    @Test
+    public void myAddTest(){
+        testSet.myAdd("testString");
+        String actual = testSet.myGetAt(0).toString();
+        String expected = "testString";
+        //String expected = "fail";//fail test
+        assertEquals("should say 'testString' at index 0", expected, actual);
+    }
+    
+    @Test
+    public void myContainsTest(){
+        String s = "testString";
+        testSet.myAdd(s);
+        boolean actual = testSet.myContains("testString");
+        boolean expected = true;
+        assertEquals("should be true, s was added", expected, actual);
+    }
+    @Test
+    public void getMySizeTest(){
+        String s = "testString";
+        testSet.myAdd(s);
+        int actual = testSet.getMySize();
+        int expected = 1;
+        assertEquals("should return 1, has only 1 thing", expected, actual);
+    }
+    @Test
+    public void myClearTest(){
+        testSet.myAdd("one");
+        testSet.myAdd("two");
+        testSet.myClear();
+        int actual = testSet.getMySize();
+        int expected = 0;
+        assertEquals("should be 0, empty myArrayList", expected, actual);
+    }
+
 }
